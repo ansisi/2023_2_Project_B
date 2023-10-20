d7a082adf6d566a14d4d7e26224ed8916f0e4f89
ansisi <sihyeona73@gmail.com>
2023-10-20 09:22:17 +0900

    Feat: 발사체 까지

--- Project_B/Assets/Script/GameManager.cs
+++ Project_B/Assets/Script/GameManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameManager : GenericSingleton<GameManager>
 {
     public int playerScore = 0;
+
     public void IncreaseScore(int amount)
     {
         playerScore += amount;
--- Project_B/Assets/Script/SampleController.cs
+++ Project_B/Assets/Script/SampleController.cs
@@ -14,6 +14,6 @@ public class SampleController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
