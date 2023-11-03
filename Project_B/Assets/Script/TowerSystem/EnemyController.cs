918249e042a29bb549aa519629c44239c9977ca8
ansisi <sihyeona73@gmail.com>
2023-11-03 09:53:32 +0900

    Feat:slowTower 제작
    안시현 장~~

--- Project_B/Assets/Script/TowerSystem/EnemyController.cs
+++ Project_B/Assets/Script/TowerSystem/EnemyController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     public float moveSpeed;
+    public bool modeEnd = true;
+    public float speedMod = 1.0f;
+    public float timeSinceStart = 0.0f;
 
     private EnemyPath thePath;          // 몬스터가 가지고 있는 path 값
     private int currentPoint;           // 지금 몇 번째 point를 향하고 있는지 변수
@@ -22,13 +25,23 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (modeEnd == false)
+        {
+            timeSinceStart -= Time.deltaTime;
+
+            if (timeSinceStart <= 0.0f)
+            {
+                speedMod = 1.0f;
+                modeEnd = true;
+            }
+          }
         if(reacheEnd == false)      // if(!reacheEnd) 도달 이전
         {
             transform.LookAt(thePath.points[currentPoint]); // 몬스터는 지금 방향을 향해서 본다. (LookAt 함수)
 
             // MoveToward 함수 (내위치, 타겟 위치, 속도값)
             transform.position =
-                Vector3.MoveTowards(transform.position, thePath.points[currentPoint].position, moveSpeed * Time.deltaTime);
+                Vector3.MoveTowards(transform.position, thePath.points[currentPoint].position, moveSpeed * Time.deltaTime * speedMod);
 
             // Vector3.Distance (A,B) 벡터의 거리 => 거리가 0.01 이하일 경우 도착했다고 간주
             if(Vector3.Distance(transform.position , thePath.points[currentPoint].position) < 0.01f)
@@ -40,5 +53,14 @@ public class EnemyController : MonoBehaviour
                 }
             }
         }
+
+
+    }
+    public void SetMode(float value)
+    {
+        modeEnd = false;
+        speedMod = value;
+        timeSinceStart = 2.0f;
+
     }
 }
